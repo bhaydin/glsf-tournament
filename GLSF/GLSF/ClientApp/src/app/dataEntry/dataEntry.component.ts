f8e7e35dc612ff4cf44cbc9be63b85330013d817
Janecek <janecekem@ad.msoe.edu>
2019-11-06 20:28:37 -0600

    Added model to website and fixed error where image chosen from file won't appear

--- GLSF/GLSF/ClientApp/src/app/dataEntry/dataEntry.component.ts
+++ GLSF/GLSF/ClientApp/src/app/dataEntry/dataEntry.component.ts
@@ -5,6 +5,8 @@ import { Router } from "@angular/router"
 import { DatePipe } from '@angular/common';
 import { CameraDialog } from './camera';
 import { MatDialog } from '@angular/material';
+import * as $ from 'jquery';
+import * as tf from '../../assets/tfjs.js';
 
 
 @Component({
@@ -25,8 +27,31 @@ export class DataEntryComponent {
 	sampleStyle = "disabled";
 	currentDate: Date = new Date();
   fishes = Fish.fishes;
+  dateConfig: any;
+  dataURL: any;
+  file: any;
+  model: any;
+  modelLocation = "../assets/FishModel/FishClassifier/model.json";
+  IMAGENET_CLASSES = {
+    0: 'Fish',
+    1: 'Not a fish',
+  };
 
 	constructor(private dialog: MatDialog, private pipe: DatePipe, private router: Router, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
+
+  ngOnInit() {
+    this.loadModel();
+  }
+
+  async loadModel() {
+    console.log("Loading model");
+    const var1 = await fetch(this.modelLocation);
+    console.log(var1);
+    this.model = await tf.loadModel(this.modelLocation);
+    console.log("Model: ");
+    console.log(this.model);
+    console.log("");
+  }
 
 	openDialog(): void {
 		const dialogRef = this.dialog.open(CameraDialog, {
@@ -35,7 +60,7 @@ export class DataEntryComponent {
 
 		dialogRef.afterClosed().subscribe(result => {
 			if (result != undefined) {
-				this.base64 = result;
+				this.base64 = result.toString();
 				this.imageAvailable = true;
 			}
 		});
@@ -45,10 +70,13 @@ export class DataEntryComponent {
 		if (image.length !== 0) {
 			const reader = new FileReader();
 			reader.readAsDataURL(image[0]);
-			reader.onload = () => {
-				this.base64 = reader.result.toString();
-				this.imageAvailable = true;
-			}
+      reader.onload = () => {
+        this.base64 = reader.result.toString();
+        this.imageAvailable = true;
+        this.dataURL = reader.result;
+      };
+
+      console.log("Read file");
 		}
 	}
 
@@ -157,8 +185,10 @@ export class DataEntryComponent {
       headers: new HttpHeaders({
           'Content-Type': 'application/json'
       })
-	  }
-	  this.http.post<Fish>(link, values, httpOptions).subscribe();
+    }
+
+    console.log(values);
+    this.predict(link, values, httpOptions);
   }
 
   private async reload() {
@@ -171,6 +201,66 @@ export class DataEntryComponent {
 
   private wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async predict(link, values, httpOptions) {
+    console.log(values);
+    console.log("Predicting");
+
+    let image = $("#selected-image").get(0); //<HTMLImageElement> this.dataURL;
+    console.log(image);
+
+    var canvas = <HTMLCanvasElement>document.getElementById("canvas"),
+      ctx = canvas.getContext("2d"),
+      img = <HTMLImageElement>(new Image()),
+      w = canvas.width / 3;
+
+    ctx.drawImage(<HTMLImageElement>image, 0, 0);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imgData.data;
+    console.log("Image data");
+    console.log(data.length);
+    console.log(canvas.width);
+    console.log(canvas.height);
+    console.log(data);
+
+    for (var i = 0; i < data.length; i += 4) {
+      data[i] = data[i] / 255.0;   // Red
+      data[i + 1] = data[i + 1] / 255.0; // Green
+      data[i + 2] = data[i + 2] / 255.0; // Blue
+    }
+
+    let tensor = tf.fromPixels(imgData)
+      .resizeNearestNeighbor([250, 250])
+      .toFloat()
+      .expandDims();
+
+    console.log(tensor.shape);
+
+    console.log("getting predictions from model");
+    let predictions = await this.model.predict(tensor).data();
+    console.log("got predictions");
+    console.log(predictions);
+
+
+    console.log("making prediction list");
+    $("#prediction-list").empty();
+    let prediction = predictions[0];
+    let valid = false;
+
+    if (prediction >= .8) {
+      valid = true;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log("Made predictions");
+
+    console.log(values);
+    values.Valid = valid;
+    console.log(values);
+
+    this.http.post<Fish>(link, values, httpOptions).subscribe();
+    console.log("Sent data to DB");
   }
 }
 
